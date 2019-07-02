3c6b404aef27d7f2afe41e492ca9179d0ad3bea6
Maxim <stat1c@ngs.ru>
2019-07-02 23:22:19 +0700

    something wrong

--- js/main.js
+++ js/main.js
@@ -88,7 +88,81 @@ address.setAttribute('value', pinMainLeft + ', ' + pinMainTop);
 
 setAttr()
 
-pinMain.addEventListener('click', function(){
+// change placeholder value
+
+var adForm = document.querySelector('.ad-form'); 
+var typeOfHousing = adForm.querySelector('#type');
+var priceInput = adForm.querySelector('#price');
+
+var changePlaceholder = function (value) {
+  if (value == 'bungalo') {
+    priceInput.placeholder = 0;
+    priceInput.min = 0;
+  } else if (value == 'flat') {
+    priceInput.placeholder = 1000;
+    priceInput.min = 1000;
+  } else if (value == 'house') {
+    priceInput.placeholder = 5000;
+    priceInput.min = 5000;
+  } else if (value == 'palace') {
+    priceInput.placeholder = 10000;
+    priceInput.min = 10000;
+  }
+};
+
+typeOfHousing.addEventListener('change', function (evt) {
+  changePlaceholder(evt.target.value);
+});
+
+// checkIn - checkOut Inputs
+
+var checkInTime = adForm.querySelector('#timein');
+var checkOutTime = adForm.querySelector('#timeout');
+
+var changeCheckInTime = function (opt, index) {
+  opt.selectedOptions[0].selected = false;
+  opt.options[index].selected = true;
+};
+
+checkInTime.addEventListener('change', function (evt) {
+  changeCheckInTime(checkOutTime, evt.target.selectedIndex);
+});
+
+checkOutTime.addEventListener('change', function (evt) {
+  changeCheckInTime(checkInTime, evt.target.selectedIndex);
+});
+
+// drag and drop
+
+pinMain.addEventListener('mousedown', function(evt){
+  evt.preventDefault();
   onClickActivate();
   addPinsToPage()
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+    
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    pinMain.style.top = (pinMain.offsetTop - shift.y) + 'px';
+    pinMain.style.left = (pinMain.offsetLeft - shift.x) + 'px';
+  };
+
   });
+  
